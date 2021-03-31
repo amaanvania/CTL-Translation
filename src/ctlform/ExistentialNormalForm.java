9b4e098c259a00632d95d3c47600c7753faf72fd
rasho95 <rasho95@my.yorku.ca>
2021-03-31 02:18:13 -0700

    skeleton for the translate classes.

---
+++ src/ctlform/ExistentialNormalForm.java
@@ -0,0 +1,13 @@
+package ctlform;
+
+import ctl.Formula;
+
+public class ExistentialNormalForm {
+	
+	public static Formula translate(Formula form) {
+		Formula existentialNormalForm = form;
+		
+		return existentialNormalForm;
+	}
+
+}
---
+++ src/ctlform/PositiveNormalForm.java
@@ -0,0 +1,13 @@
+package ctlform;
+
+import ctl.Formula;
+
+public class PositiveNormalForm {
+	
+	public static Formula translate(Formula form) {
+		Formula existentialNormalForm = form;
+		
+		return existentialNormalForm;
+	}
+
+}
