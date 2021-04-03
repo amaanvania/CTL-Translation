e82a8863a189f94da66e309f231fac4eeaba7d78
rasho95 <rasho95@my.yorku.ca>
2021-04-03 01:15:02 -0700

    made translate method in each conversion type for universal access.

--- src/ctlform/PositiveNormalForm.java
+++ src/ctlform/PositiveNormalForm.java
@@ -12,40 +12,11 @@ import parser.CTLParser;
 
 public class PositiveNormalForm {
 	
-	public static Formula translate(Formula form) {
-		Formula tempformula = form;
+	public static Formula translate(Formula form) {		
+		String tempString = form.toString();
 		
-		String tempString = tempformula.toString();
-		
-		Generator gen = new Generator();
-		
-		//take in a formula
-		
-		//dissect it into its parts
-			//how to dissect by the different sub-translations?
-			//how to dissect distinct atomics from the original formula?
-		
-		//recursively perform the translations on each part starting from the inner and working towards the outer.
-			//
-		
-		//combine it all together
-		
-		//apply visitor classes to go from string to Formula type
-		
-		String newstring = "";
-		
-		Formula positiveNormalForm = gen.visit(parseCtl(newstring));
+		Formula positiveNormalForm = PositiveTranslation.translation(tempString);
 		
 		return positiveNormalForm;
 	}
-	
-	private static ParseTree parseCtl(String ctlFormula) {
-		CharStream input = CharStreams.fromString(ctlFormula);
-		CTLLexer lexer = new CTLLexer(input);
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		CTLParser parser = new CTLParser(tokens);
-		ParseTree tree = parser.root();
-		return tree;
-	}
-
 }
